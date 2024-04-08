942d3d73bf2b40f6030ed2c79a001c8b4a898b29
Anchaeyeon <s2328@e-mirim.hs.kr>
2024-04-08 14:59:45 +0900

    생성자

--- 2024-CPP-201/03-01 구조체와 클래스.cpp
+++ 2024-CPP-201/03-01 구조체와 클래스.cpp
@@ -3,14 +3,20 @@
 using namespace std;
 
 // 클래스는 default가 private
-class Student {
+class Student{
+public:
+	// 생성자 : 객체를 생성할 때 호출되는 함수
+	// 생성자는 반환형이 없다
+	Student(int hakbun, string name) {
+		hakbun_ = hakbun;
+		name_ = name;
+	}
+private:
 	int hakbun_;
 	string name_;
 };
 
 void main(void)
 {
-	Student juyoung;
-	juyoung.hakbun_ = 2115;
-	juyoung.name_ = "윤주영";
+	Student juyoung = Student(2115, "윤주영");
 }

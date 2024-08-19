6a9f6d5f2fab8a87feab168b6627b23d06ea8798
Anchaeyeon <s2328@e-mirim.hs.kr>
2024-08-19 14:48:34 +0900

    아주 무난한 다형성

--- 2024-CPP-201/05-02 다형성.cpp
+++ 2024-CPP-201/05-02 다형성.cpp
@@ -7,8 +7,8 @@ public :
 	Animal(string name, unsigned int age)
 		: name_(name), age_(age)
 	{
-		cout << "이름" << name_ << endl;
-		cout << "나이" << age_ << endl;
+		cout << "이름 " << name_ << endl;
+		cout << "나이 " << age_ << endl;
 	}
 
 	void bark()
@@ -34,6 +34,27 @@ private:
 class Human : public Animal
 {
 public:
+	//부모(Animal) 생성자가 먼저 호출된다.
+	Human(string name, unsigned int age, bool right)
+		: Animal(name, age), right_(right) //멤버변수(매개변수)
+	{
+		cout << "인권 존재 여부 " << right_ << endl;
+	}
+
+	void bark()
+	{
+		cout << "톡톡" << endl;
+	}
+
+	void sleep()
+	{
+		cout << "쿨쿨" << endl;
+	}
+
+	void eat()
+	{
+		cout << "냠냠" << endl;
+	}
 
 private:
 	bool right_;
@@ -47,5 +68,12 @@ void main(void)
 	ani->eat();
 	ani->sleep();
 
+	Human* hum = new Human("지혜레이디", 18, true);
+
+	hum->bark();
+	hum->eat();
+	hum->sleep();
+
+	delete hum;
 	delete ani;
 }

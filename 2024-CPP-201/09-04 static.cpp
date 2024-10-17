c9b5282f0aab6ea2ad0f966fd46df4cac84c4446
Anchaeyeon <s2328@e-mirim.hs.kr>
2024-10-18 08:47:45 +0900

    [09-04] static 멤버 함수에는 일반 멤버변수가 들어갈 수 없다

--- 2024-CPP-201/09-04 static.cpp
+++ 2024-CPP-201/09-04 static.cpp
@@ -15,7 +15,11 @@ public:
 	}
 
 	static int* get_address_num(void) { return &num_; }
-	static int get_num(void) { return num_; }
+	static int get_num(void) {
+		// gam_++;
+		// static 멤버 함수에는 일반 멤버변수가 들어갈 수 없다
+		return num_;
+	}
 	int* get_address_gam(void) { return &gam_; }
 	int get_gam(void) { return gam_; }
 };
